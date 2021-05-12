7de61772e50aa3bd1c7942f574626a1367ac1e4a
matthieu CONTI <mconti@student.42.fr>
2021-05-12 22:13:11 +0200

    cube rotation on world axis

--- src/Components/Three/Cube.jsx
+++ src/Components/Three/Cube.jsx
@@ -6,6 +6,7 @@ import useEventListener from 'Hooks/useEventListener';
 import { SPEED_CUBE_ROTATION } from 'helpers/constans/three';
 
 import CubeBox from './CubeBox';
+import { Vector3 } from 'three';
 
 const createCubeBoxFromIndex = (index) => <CubeBox
   key={index}
@@ -21,8 +22,10 @@ const Cube = () => {
   // ROTATE CUBE
   const [ needRotate, setNeedRotate ] = useState(false);
   useMouseMove(needRotate, (x, y) => {
-    cube.current.rotation.x += (y * SPEED_CUBE_ROTATION);
-    cube.current.rotation.y += (x * SPEED_CUBE_ROTATION);
+    // cube.current.rotateX(y * SPEED_CUBE_ROTATION);
+    // cube.current.rotateY(x * SPEED_CUBE_ROTATION);
+    cube.current.rotateOnWorldAxis(new Vector3(1, 0, 0), (y * SPEED_CUBE_ROTATION));
+    cube.current.rotateOnWorldAxis(new Vector3(0, 1, 0), (x * SPEED_CUBE_ROTATION));
   });
   useEventListener('mouseup', () => { setNeedRotate(false); });
 
